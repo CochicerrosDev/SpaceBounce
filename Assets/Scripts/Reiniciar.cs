5f028eed37e050c153f6a31eb3d15805dc854382
Doblej12 <jjcruzcabello@gmail.com>
2014-11-18 19:10:10 +0100

    reinicio despues el gol añadido

---
+++ Assets/Scripts/Reiniciar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reiniciar : MonoBehaviour { 
+
+	public GameObject jugador1 = null;
+	public GameObject jugador2 = null;
+	public GameObject bola = null; 
+
+	void OnTriggerEnter2D(Collider2D collider){
+		 if (collider.gameObject.tag == "GolA" || collider.gameObject.tag == "GolB") {
+			jugador1.gameObject.transform.position = new Vector3(-6.640419f,0f,0f);
+			jugador1.rigidbody2D.velocity = new Vector3(0f,0f,0f);
+			jugador2.gameObject.transform.position = new Vector3(6.508911f,0f,0f);
+			jugador2.rigidbody2D.velocity = new Vector3(0f,0f,0f);
+			bola.gameObject.transform.position = new Vector3(0f,0f,0f);
+			bola.rigidbody2D.velocity = new Vector3(0f,0f,0f);
+		}
+	}
+}
